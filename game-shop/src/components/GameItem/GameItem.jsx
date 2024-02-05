e3a9c762936676581f472c5d84bef25aeda6b4dd
chel12 <sema.chel12@gmail.com>
2024-02-05 17:57:44 +0300

    components:GameCover+GameItem

--- game-shop/src/components/GameItem/GameItem.jsx
+++ game-shop/src/components/GameItem/GameItem.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import GameCover from '../GameCover/GameCover';
+import './GameItem.css';
+const GameItem = ({ game }) => {
+	return (
+		<div className="game-item">
+			<GameCover image={game.image} />
+			<div className="game-item__details">
+				<span className="game-item__title">{game.title}</span>
+				<div className="game-item__genre">
+					{game.genres.map((genre) => genre)}
+				</div>
+				<div className="game-item__buy">Buy</div>
+			</div>
+		</div>
+	);
+};
 
-const GameItem = () => {
-  return (
-	 <div>GameItem</div>
-  )
-}
-
-export default GameItem
+export default GameItem;
